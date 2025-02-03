90d6534dc055384ab82326eab9f88f13a01b0645
prospermbuma <prospermbuma@gmail.com>
2025-02-03 16:55:04 +0300

    Added users.js for learning purpose

---
+++ assets/js/users.js
@@ -0,0 +1,10 @@
+// THE USE OF CONSOLE TABLE
+
+const users = [
+    { name: "Prosper Mbuma", age: 30, role: "Software Engineer" },
+    { name: "Joseph Mbunma", age: 26, role: "Site Engineer" },
+    { name: "Gladness Mbuma", age: 22, role: "Social Policy Advisor" },
+    { name: "Victoria Mbuma", age: 32, role: "Social Worker" }
+]
+
+console.table(users);
